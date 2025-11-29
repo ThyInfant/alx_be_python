3ac14e0de45c69da5be3b5c57208f1d8dcec837d
Rob Tade <robtade4@gmail.com>
2025-11-29 17:10:40 +0300

    Fix indentation

--- programming_paradigm/robust_division_calculator.py
+++ programming_paradigm/robust_division_calculator.py
@@ -1,15 +1,13 @@
 def safe_divide(numerator, denominator):
-    # Try converting to float
-    try:
-        num = float(numerator)
-        den = float(denominator)
-    except ValueError:
-        return "Error: Please enter numeric values only."
+  try:
+      num = float(numerator)
+      den = float(denominator)
+  except ValueError:
+      return "Error: Please enter numeric values only."
 
-    # Try performing division
-    try:
-        result = num / den
-    except ZeroDivisionError:
-        return "Error: Cannot divide by zero."
+  try:
+      result = num / den
+  except ZeroDivisionError:
+      return "Error: Cannot divide by zero."
 
-    return f"The result of the division is {result}"
+  return f"The result of the division is {result}"
